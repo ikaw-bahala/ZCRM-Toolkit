b65e2c7a8909de8db3c1e4df19791eb6d4f49afd
Marjose <hypernate1@gmail.com>
2022-10-21 15:38:27 +0800

    WIP

--- app/traits/crm/hasCRMConfig.php
+++ app/traits/crm/hasCRMConfig.php
@@ -2,11 +2,14 @@
 
 namespace App\traits\crm;
 
+use com\zoho\api\authenticator\OAuthBuilder;
 use com\zoho\api\authenticator\store\FileStore;
 use com\zoho\api\logger\Levels;
 use com\zoho\api\logger\LogBuilder;
 use com\zoho\crm\api\dc\USDataCenter;
 use com\zoho\crm\api\exception\SDKException;
+use com\zoho\crm\api\InitializeBuilder;
+use com\zoho\crm\api\SDKConfigBuilder;
 use com\zoho\crm\api\UserSignature;
 use Illuminate\Support\Facades\Storage;
 
@@ -29,7 +32,62 @@ trait hasCRMConfig
         * Available Environments: PRODUCTION(), DEVELOPER(), SANDBOX()
         */
         $environment = USDataCenter::PRODUCTION();
+
+        /*
+        * Create a Token instance that requires the following
+        * clientId -> OAuth client id.
+        * clientSecret -> OAuth client secret.
+        * refreshToken -> REFRESH token.
+        * accessToken -> Access token.
+        * grantToken -> GRANT token.
+        * id -> User unique id.
+        * redirectURL -> OAuth redirect URL.
+        */
+        //Create a Token instance
+        // if refresh token is available
+        // The SDK throws an exception, if the given id is invalid.
+        $token = (new OAuthBuilder())
+            ->clientId(env('CRM_CLIENT_ID', null))
+            ->clientSecret(env('CRM_CLIENT_SECRET', null))
+            ->grantToken(env('CRM_GRANT_TOKEN', null))
+            ->redirectURL(env('CRM_REDIRECT_URL'))
+            ->build();
+        /*
+         * TokenStore can be any of the following
+         * DB Persistence - Create an instance of DBStore
+         * File Persistence - Create an instance of FileStore
+         * Custom Persistence - Create an instance of CustomStore
+        */
         $tokenstore = new FileStore(Storage::disk('local')->path('auth/php_sdk_token.txt'));
+
+        $sdkConfig = (new SDKConfigBuilder())
+            ->autoRefreshFields(false)
+            ->pickListValidation(false)
+            ->sslVerification(false)
+            ->connectionTimeout(2)
+            ->timeout(2)
+            ->build();
+
+        /*
+           * Set the following in InitializeBuilder
+           * user -> UserSignature instance
+           * environment -> Environment instance
+           * token -> Token instance
+           * store -> TokenStore instance
+           * SDKConfig -> SDKConfig instance
+           * resourcePath -> resourcePath - A String
+           * logger -> Log instance (optional)
+           * requestProxy -> RequestProxy instance (optional)
+         */
+        (new InitializeBuilder())
+            ->user($user)
+            ->environment($environment)
+            ->token($token)
+            ->store($tokenstore)
+            ->SDKConfig($sdkConfig)
+            ->resourcePath(getcwd())
+            ->logger($logger)
+            ->initialize();
 
     }
 }
