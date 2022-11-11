8260ad993c01b1a71413defca1813ecc55069f31
Marjose <hypernate1@gmail.com>
2022-11-11 16:36:21 +0800

    WIP

--- app/traits/migration/Migrate.php
+++ app/traits/migration/Migrate.php
@@ -5,6 +5,7 @@ namespace App\traits\migration;
 use App\Helper\CRMSDK;
 use App\traits\crm\hasCRMConfig;
 use Illuminate\Support\Facades\Storage;
+use Spatie\SimpleExcel\SimpleExcelReader;
 
 trait Migrate
 {
@@ -21,8 +22,20 @@ trait Migrate
         $this->ZCRMconfig();
 
         $path = $this->getCSV($module);
-        $getFieldMappingData = (new MigrationFieldMapping($module))->check()->getFieldMapping();
-        dump($getFieldMappingData);
+        $getFieldMappingData = collect((new MigrationFieldMapping($module))->check()->getFieldMapping());
+        $getFieldMappingData->each(function($properties) use ($path) {
+            $fieldMapping = collect($properties);
+            $rows = SimpleExcelReader::create($path)->getRows();
+            $rows->each(function (array $csvProperties) use ($fieldMapping){
+
+
+            });
+
+            dump($fieldMapping->values());
+            dump($fieldMapping->keys());
+
+        });
+        dump($getFieldMappingData->count());
 
 //        dump((new CRMSDK())->getModuleRecordCount('Leads'));
 
