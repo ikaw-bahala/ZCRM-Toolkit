6fcd349bea335090093b3d9e336e0f0d21ad7cca
Marjose <hypernate1@gmail.com>
2022-11-11 18:06:17 +0800

    WIP

--- app/traits/migration/Migrate.php
+++ app/traits/migration/Migrate.php
@@ -4,7 +4,11 @@ namespace App\traits\migration;
 
 use App\Helper\CRMSDK;
 use App\traits\crm\hasCRMConfig;
-use Illuminate\Support\Facades\Storage;
+use com\zoho\crm\api\record\BodyWrapper;
+use com\zoho\crm\api\record\Record;
+use com\zoho\crm\api\record\RecordOperations;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 use Spatie\SimpleExcel\SimpleExcelReader;
 
 trait Migrate
@@ -12,30 +16,46 @@ trait Migrate
     use hasCSVFile;
     use hasCRMConfig;
 
-    private array $data;
-
     /**
      * @throws \Exception
      */
     public function startMigration($self, $module): void
     {
         $this->ZCRMconfig();
+        //Get instance of RecordOperations Class that takes moduleAPIName as parameter
+        $recordOperations = new RecordOperations();
+
+        //Get instance of BodyWrapper Class that will contain the request body
+        $bodyWrapper = new BodyWrapper();
+        //List of Record instances
+        $records = new Collection();
+
+        //Get instance of Record Class
+        $record1 = new Record();
 
         $path = $this->getCSV($module);
         $getFieldMappingData = collect((new MigrationFieldMapping($module))->check()->getFieldMapping());
-        $getFieldMappingData->each(function($properties) use ($path) {
-            $fieldMapping = collect($properties);
-            $rows = SimpleExcelReader::create($path)->getRows();
-            $rows->each(function (array $csvProperties) use ($fieldMapping){
-
-
+        $rows = SimpleExcelReader::create($path)->getRows();
+        $rows->each(function (array $csvProperties) use ($records, $record1, $getFieldMappingData){
+            $getFieldMappingData->each(function($properties) use ($records, $csvProperties, $record1) {
+                $fieldMapping = collect($properties);
+                dump("{$fieldMapping->get('crm_field_name')}", "{$csvProperties[$fieldMapping->get('csv_column')]}");
+                $record1->addKeyValue("{$fieldMapping->get('crm_field_name')}", "{$csvProperties[$fieldMapping->get('csv_column')]}");
+               $records->push($record1);
+                });
             });
-
-            dump($fieldMapping->values());
-            dump($fieldMapping->keys());
-
-        });
-        dump($getFieldMappingData->count());
+//        $getFieldMappingData->each(function($properties) use ($path) {
+//            $fieldMapping = collect($properties);
+//            $rows = SimpleExcelReader::create($path)->getRows();
+//            $rows->each(function (array $csvProperties) use ($fieldMapping){
+//                if($csvProperties[$fieldMapping->value('csv_column')] !== null)
+//                {
+//                    $record1->addKeyValue($fieldMapping->value('crm_field_name'), $csvProperties[$fieldMapping->value('csv_column')]);
+//                }
+//            });
+//        });
+        Log::info("test",$records->all());
+        dump($records->all());
 
 //        dump((new CRMSDK())->getModuleRecordCount('Leads'));
 
